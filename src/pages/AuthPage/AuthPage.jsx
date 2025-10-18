8456ed07f3b71a0cf1c1525fb0ac21ca367b3450
Oleksandr Veryk <48521735+alexveryk@users.noreply.github.com>
2025-10-18 11:23:29 +0000

    Created Auth form

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,13 @@
 import { AuthIntro } from "../../components/AuthIntro/AuthIntro";
 import { Menu } from "../../components/Menu/Menu";
+import { UserRegistration } from "../../components/UserRegistration/UserRegistration";
 
 export const AuthPage = () => {
   return (
     <>
       <Menu />
-      <AuthIntro />
+      {/* <AuthIntro /> */}
+      <UserRegistration/>
     </>
   );
 };
